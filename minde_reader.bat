f8bfa203f0f1510d45cf65cebb52ee3b99bad3ce
JR-EMAD <152868789+JR-EMAD@users.noreply.github.com>
2024-03-17 20:38:20 +0330

    Add files via upload

---
+++ minde_reader.bat
@@ -0,0 +1,25 @@
+@echo off
+echo ---------------------------------------
+echo ---      jR_emad minde reader!      ---
+echo ---  *****************************  ---
+echo ---  1.enter a word or number       ---
+echo ---  2.and wait!                    ---
+echo ---------------------------------------
+echo.
+echo.
+set /p a="please enter your word or number: "
+timeout 1 /nobreak > nul
+echo reading your minde!!!!. please wait
+timeout 2 /nobreak > nul
+echo *
+timeout 1 /nobreak > nul
+echo *
+timeout 1 /nobreak > nul
+echo *
+timeout 1 /nobreak > nul
+echo *
+timeout 1 /nobreak > nul
+echo i read your minde and "%a%" in your minde
+timeout 3 /nobreak > nul 
+echo thnak for useing. by jR_emad
+timeout 9999 /nobreak > nul
